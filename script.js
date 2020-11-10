f270f1d97fbc18bb3e8b76ae8e20bd1317d67e53
DavidPinedaHidalgo <davidpinedahidalgo@gmail.com>
2020-11-10 19:04:10 +0100

    2 files modified

--- script.js
+++ script.js
@@ -11,5 +11,70 @@
 window.addEventListener("load", onLoad);
 
 function onLoad() {
-  console.log("hi");
+  addElementClass();
+
+  deletePairLi();
+
+  createList2();
+
+  disableButton5();
+}
+
+function addElementClass() {
+  let spanSelect = document.querySelector(".selected");
+  let parentEle = spanSelect.parentElement.parentElement;
+  let nodeList = parentEle.childNodes;
+
+  let counter = 1;
+  for (var i = 0; i < nodeList.length; i++) {
+    if (nodeList[i] instanceof HTMLLIElement) {
+      let il = nodeList[i].children;
+      il[0].classList.add("element-" + counter);
+      counter++;
+    }
+  }
+}
+
+function deletePairLi() {
+  let spanSelect = document.querySelector(".selected");
+  let parentEle = spanSelect.parentElement.parentElement;
+  let nodeList = parentEle.childNodes;
+
+  nodeList[3].remove();
+  nodeList[6].remove();
+}
+
+function createList2() {
+  let list2 = document.getElementById("list2");
+
+  //Creacion de los botones con la misma jerarquia y clases.
+  let button1 = document.createElement("BUTTON");
+  button1.classList.add("element-1");
+  button1.innerHTML = "texto ejemplo 1";
+
+  let button3 = document.createElement("BUTTON");
+  button3.classList.add("element-3");
+  button3.innerHTML = "texto ejemplo 3";
+
+  let button5 = document.createElement("BUTTON");
+  button5.classList.add("element-5");
+  button5.innerHTML = "texto ejemplo 5";
+  button5.setAttribute("id", "button5");
+
+  //Creacion de las etiquetas li con los botones y agregacion a las li al ul.
+  let li1 = document.createElement("LI");
+  li1.appendChild(button1);
+  list2.appendChild(li1);
+
+  let li3 = document.createElement("LI");
+  li3.appendChild(button3);
+  list2.appendChild(li3);
+
+  let li5 = document.createElement("LI");
+  li5.appendChild(button5);
+  list2.appendChild(li5);
+}
+
+function disableButton5() {
+  let btn5 = (document.getElementById("button5").disabled = true);
 }
